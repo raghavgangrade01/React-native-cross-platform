f95738e219b7f9bcbda05b3f3b9a9c793d9f8462
raghavgangrade01 <gangraderaghav7@gmail.com>
2025-02-18 18:02:55 +0530

    test121

--- src/screens/admin.tsx
+++ src/screens/admin.tsx
@@ -4,12 +4,6 @@ import React from 'react'
 export default function admin() {
   return (
     <View>
-      <Text>anjnadd</Text>
-      <Text>admin</Text>
-      <Text>admin</Text>
-      <Text>admin</Text>
-      <Text>admin</Text>
-      <Text>admin</Text>
       <Text>admin</Text>
     </View>
   )
